76a80309efbb95dd7489ac5cb7daa93bff7853af
lawrat <salllawratou248@gmail.com>
2024-02-03 19:00:12 +0000

    my commit

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const addr = "localhost"
@@ -15,6 +17,9 @@ const FileForContact = "contact.json"
 const FileForMeet = "meet.json"
 const FileForEntreprise = "entreprise.json"
 const FileForDomicile = "domicile.json"
+
+const commentFile = "commentaire.json"
+const devisFile = "devis.json"
 
 type modeleStruct struct {
 	Nom         string `json:"nom"`
@@ -65,19 +70,62 @@ type modeleDomicile struct {
 	Piscine      string   `json:"piscine"`
 	Entrepot     string   `json:"entrepot"`
 }
+type modelecommentaire struct {
+	Username    string `json:"user"`
+	Commentaire string `json:"commenter"`
+}
+type modeleDevis struct {
+	Nom     string `json:"nom"`
+	Prenom  string `json:"prenom"`
+	Email   string `json:"email"`
+	Numero  string `json:"numero"`
+	Message string `json:"message"`
+}
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	t, err := template.ParseFiles("./templates/" + tmpl + ".html")
 	if err != nil {
 		fmt.Fprint(w, "MODELE INTROUVABLE...")
+		return
 	}
-	t.Execute(w, nil)
+
+	// Vérifier si le fichier JSON existe
+	if _, err := os.Stat(commentFile); os.IsNotExist(err) {
+		// Si le fichier n'existe pas, afficher la page d'accueil sans les commentaires
+		t.Execute(w, data)
+		return
+	}
+
+	// Charger les commentaires depuis le fichier JSON
+	commentActuel, err := ioutil.ReadFile(commentFile)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier JSON de commentaires :", err)
+		http.Error(w, "Erreur lors de la lecture du fichier JSON de commentaires", http.StatusInternalServerError)
+		return
+	}
+
+	var comments []modelecommentaire
+
+	// Si le fichier JSON existe déjà, décodez-le
+	err = json.Unmarshal(commentActuel, &comments)
+	if err != nil {
+		fmt.Println("Erreur lors du décodage du fichier JSON de commentaires :", err)
+		http.Error(w, "Erreur lors du décodage du fichier JSON de commentaires", http.StatusInternalServerError)
+		return
+	}
+
+	// Ajouter les commentaires au contexte de rendu
+	data = map[string]interface{}{
+		"Comments": comments,
+	}
+
+	t.Execute(w, data)
 }
 
 func accueil(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		renderTemplate(w, "accueil")
+		renderTemplate(w, "accueil", "")
 	case "POST":
 		requestType := r.FormValue("request_type")
 		switch requestType {
@@ -194,6 +242,58 @@ func accueil(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Données du formulaire ajoutées avec succès à", FileForMeet)
 			http.Redirect(w, r, "/mercimeet", http.StatusSeeOther)
 			return
+		case "commentaire":
+			commentOnly := modelecommentaire{
+				Username:    getUsernameFromRequest(r),
+				Commentaire: r.FormValue("commenter"),
+			}
+
+			// Vérifier si le fichier JSON existe
+			if _, err := os.Stat(commentFile); os.IsNotExist(err) {
+				// Le fichier n'existe pas, initialiser avec un tableau vide
+				err := ioutil.WriteFile(commentFile, []byte("[]"), 0644)
+				if err != nil {
+					http.Error(w, "Erreur lors de la création du fichier JSON", http.StatusInternalServerError)
+					return
+				}
+			}
+
+			// Lire le contenu actuel du fichier JSON
+			commentActuel, err := ioutil.ReadFile(commentFile)
+			if err != nil {
+				http.Error(w, "Erreur lors de la lecture du fichier JSON", http.StatusInternalServerError)
+				return
+			}
+
+			var comments []modelecommentaire
+
+			// Si le fichier JSON existe déjà, décodez-le
+			err = json.Unmarshal(commentActuel, &comments)
+			if err != nil {
+				http.Error(w, "Erreur lors du decodage du fichier JSON", http.StatusInternalServerError)
+				return
+			}
+
+			// Ajouter le nouvel utilisateur à la liste existante
+			comments = append(comments, commentOnly)
+
+			// Encodez la liste mise à jour en JSON
+			commentJSON, err := json.Marshal(comments)
+			if err != nil {
+				http.Error(w, "Erreur de conversion", http.StatusInternalServerError)
+				return
+			}
+
+			// Écrire la liste mise à jour dans le fichier JSON
+			err = ioutil.WriteFile(commentFile, commentJSON, 0644)
+			if err != nil {
+				http.Error(w, "Erreur de conversion", http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Println("Commentaire ajouté avec succès à", commentFile)
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
 		default:
 			fmt.Fprint(w, "requette non prise en charge")
 		}
@@ -201,20 +301,51 @@ func accueil(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Méthode non prise en charge")
 	}
 }
+func getUsernameFromRequest(r *http.Request) string {
+	// Récupérer l'en-tête "Authorization" de la requête
+	authHeader := r.Header.Get("Authorization")
+
+	// Vérifier si l'en-tête est présent
+	if authHeader != "" {
+		// Exemple : "Basic dXNlcjpwYXNzd29yZA=="
+		// Ici, nous supposons un encodage Basic, mais cela dépendra du type d'authentification utilisé
+		// Vous devrez peut-être définir votre propre logique d'extraction du nom d'utilisateur
+		// en fonction du type d'authentification que vous utilisez
+		// Veuillez noter que l'authentification de base (Basic Auth) transmet le nom d'utilisateur et le mot de passe codés en base64
+		// Vous devrez décoder cela si c'est ce que vous utilisez
+		// Ne pas utiliser l'authentification de base avec des mots de passe non cryptés dans un environnement de production
+
+		// Exemple d'extraction du nom d'utilisateur de l'en-tête Authorization (Base64)
+		decoded, err := base64.StdEncoding.DecodeString(authHeader)
+		if err == nil {
+			credentials := strings.Split(string(decoded), ":")
+			if len(credentials) > 0 {
+				return credentials[0]
+			}
+		}
+	}
+
+	// Si l'en-tête "Authorization" n'est pas présent ou ne contient pas d'information sur le nom d'utilisateur,
+	// vous pouvez explorer d'autres en-têtes HTTP ou d'autres méthodes en fonction de vos besoins.
+	// Notez que ces informations ne sont pas fiables et peuvent être absentes en fonction des configurations du client.
+
+	// Retourner "Anonyme" par défaut
+	return "Anonyme"
+}
 func merci(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "merci")
+	renderTemplate(w, "merci", "")
 }
 
 func mercireservation(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "mercireservation")
+	renderTemplate(w, "mercireservation", "")
 }
 func mercimeet(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "mercimeet")
+	renderTemplate(w, "mercimeet", "")
 }
 func services(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		renderTemplate(w, "services")
+		renderTemplate(w, "services", "")
 	case "POST":
 		requestType := r.FormValue("request_type")
 		switch requestType {
@@ -282,12 +413,12 @@ func services(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func apropos(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "a-propos")
+	renderTemplate(w, "a-propos", "")
 }
 func entreprise(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		renderTemplate(w, "entreprise")
+		renderTemplate(w, "entreprise", "")
 	case "POST":
 		requestType := r.FormValue("request_type")
 		switch requestType {
@@ -363,7 +494,7 @@ func entreprise(w http.ResponseWriter, r *http.Request) {
 func domicile(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		renderTemplate(w, "domicile")
+		renderTemplate(w, "domicile", "")
 	case "POST":
 		requestType := r.FormValue("request_type")
 		switch requestType {
@@ -436,12 +567,136 @@ func domicile(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func contact(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "contact")
+	renderTemplate(w, "contact", "")
 }
 
 func devis(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "devis")
+	switch r.Method {
+	case "GET":
+		renderTemplate(w, "devis", "")
+	case "POST":
+
+		requestType := r.FormValue("request_type")
+		switch requestType {
+		case "devis":
+			devi := modeleDevis{
+				Nom:     r.FormValue("nom"),
+				Prenom:  r.FormValue("prenom"),
+				Email:   r.FormValue("email"),
+				Numero:  r.FormValue("numero"),
+				Message: r.FormValue("message"),
+			}
+
+			// Vérifier si le fichier JSON existe
+			if _, err := os.Stat(devisFile); os.IsNotExist(err) {
+				// Le fichier n'existe pas, initialiser avec un tableau vide
+				err := ioutil.WriteFile(devisFile, []byte("[]"), 0644)
+				if err != nil {
+					http.Error(w, "Erreur lors de la création du fichier JSON", http.StatusInternalServerError)
+					return
+				}
+			}
+
+			// Lire le contenu actuel du fichier JSON
+			devisActuel, err := ioutil.ReadFile(devisFile)
+			if err != nil {
+				http.Error(w, "Erreur lors de la lecture du fichier JSON", http.StatusInternalServerError)
+				return
+			}
+
+			var devis []modeleDevis
+
+			// Si le fichier JSON existe déjà, décodez-le
+			err = json.Unmarshal(devisActuel, &devis)
+			if err != nil {
+				http.Error(w, "Erreur lors du decodage du fichier JSON", http.StatusInternalServerError)
+				return
+			}
+
+			// Ajouter le nouvel utilisateur à la liste existante
+			devis = append(devis, devi)
+
+			// Encodez la liste mise à jour en JSON
+			clientJSON, err := json.Marshal(devis)
+			if err != nil {
+				http.Error(w, "Erreur de conversion", http.StatusInternalServerError)
+				return
+			}
+
+			// Écrire la liste mise à jour dans le fichier JSON
+			err = ioutil.WriteFile(devisFile, clientJSON, 0644)
+			if err != nil {
+				http.Error(w, "Erreur de conversion", http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Println("Données du formulaire ajoutées avec succès à", devisFile)
+			http.Redirect(w, r, "/merci", http.StatusSeeOther)
+			return
+		}
+	}
 }
+
+// func commenter(w http.ResponseWriter, r *http.Request) {
+// 	switch r.Method {
+// 	case "GET":
+// 		renderTemplate(w, "commenter", "")
+// 	case "POST":
+// 		commentOnly := modelecommentaire{
+// 			Username:    r.FormValue("username"),
+// 			Commentaire: r.FormValue("commentaire"),
+// 		}
+
+// 		// Vérifier si le fichier JSON existe
+// 		if _, err := os.Stat(comment); os.IsNotExist(err) {
+// 			// Le fichier n'existe pas, initialiser avec un tableau vide
+// 			err := ioutil.WriteFile(comment, []byte("[]"), 0644)
+// 			if err != nil {
+// 				http.Error(w, "Erreur lors de la création du fichier JSON", http.StatusInternalServerError)
+// 				return
+// 			}
+// 		}
+
+// 		// Lire le contenu actuel du fichier JSON
+// 		commentActuel, err := ioutil.ReadFile(comment)
+// 		if err != nil {
+// 			http.Error(w, "Erreur lors de la lecture du fichier JSON", http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		var comments []modelecommentaire
+
+// 		// Si le fichier JSON existe déjà, décodez-le
+// 		err = json.Unmarshal(commentActuel, &comments)
+// 		if err != nil {
+// 			http.Error(w, "Erreur lors du décodage du fichier JSON", http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		// Ajouter le nouveau commentaire à la liste existante
+// 		comments = append(comments, commentOnly)
+
+// 		// Encodez la liste mise à jour en JSON
+// 		commentJSON, err := json.Marshal(comments)
+// 		if err != nil {
+// 			http.Error(w, "Erreur de conversion", http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		// Écrire la liste mise à jour dans le fichier JSON
+// 		err = ioutil.WriteFile(comment, commentJSON, 0644)
+// 		if err != nil {
+// 			http.Error(w, "Erreur de conversion", http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		fmt.Println("Commentaire ajouté avec succès à", comment)
+// 		return
+// 	default:
+// 		fmt.Fprint(w, "Méthode non prise en charge")
+// 	}
+// }
+
 func main() {
 	http.HandleFunc("/", accueil)
 	http.HandleFunc("/merci", merci)
@@ -453,6 +708,7 @@ func main() {
 	http.HandleFunc("/domicile", domicile)
 	http.HandleFunc("/contact", contact)
 	http.HandleFunc("/devis", devis)
+	// http.HandleFunc("/commenter", commenter)
 
 	fmt.Printf("Serveur écoute sur http://%s%s\n", addr, port)
 	http.ListenAndServe(port, nil)
